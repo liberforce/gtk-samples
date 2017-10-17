4ede584b6099aad1c890ce3bddb5c965cc3f9c2c
Luis Menina <liberforce@freeside.fr>
2017-10-17 19:38:31 +0200

    Initial revision

---
+++ gtk3-test.c
@@ -0,0 +1,43 @@
+ 
+ 
+#include <gtk/gtk.h>
+ 
+static void create_win32_native_file_chooser(GtkWidget *widget, gpointer user_data)  ;
+ 
+static void create_win32_native_file_chooser(GtkWidget *widget, gpointer user_data) {
+ 
+  GtkWidget *window = gtk_widget_get_toplevel(widget) ;
+ 
+  GtkFileChooserNative *win32_native_file_chooser = gtk_file_chooser_native_new("title", 
+                                                   GTK_WINDOW(window), 
+                                                  GTK_FILE_CHOOSER_ACTION_SAVE, // C'est ça qui bug.
+                                                  "accept label",
+                                                  "cancel label") ; 
+ 
+  gtk_native_dialog_show(GTK_NATIVE_DIALOG(win32_native_file_chooser)) ;
+ 
+  gtk_native_dialog_destroy(GTK_NATIVE_DIALOG(win32_native_file_chooser)) ;
+ 
+  return ;
+ 
+}
+ 
+ 
+int main (int argc, char *argv[]) {
+ 
+  gtk_init(&argc, &argv) ;
+ 
+  GtkWidget *window, *button ;
+ 
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL) ;
+ 
+  button= gtk_button_new_with_label("test") ;
+ 
+  g_signal_connect(button, "clicked", G_CALLBACK(create_win32_native_file_chooser) , NULL);
+ 
+  gtk_container_add(GTK_CONTAINER(window), button) ;
+ 
+  gtk_main() ;
+ 
+  return 0 ;
+}
