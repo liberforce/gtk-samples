985a56c029adaba8fca4f2b5828c8fc5f0defe93
Luis Menina <liberforce@freeside.fr>
2017-10-18 09:24:24 +0200

    Fix indentation

--- gtk3-test.c
+++ gtk3-test.c
@@ -2,39 +2,32 @@
  
 #include <gtk/gtk.h>
  
-static void create_win32_native_file_chooser(GtkWidget *widget, gpointer user_data)  ;
- 
-static void create_win32_native_file_chooser(GtkWidget *widget, gpointer user_data) {
- 
-  GtkWidget *window = gtk_widget_get_toplevel(widget) ;
- 
-  GtkFileChooserNative *win32_native_file_chooser = gtk_file_chooser_native_new("title", 
-                                                   GTK_WINDOW(window), 
-                                                  GTK_FILE_CHOOSER_ACTION_SAVE, // C'est ça qui bug.
-                                                  "accept label",
-                                                  "cancel label") ; 
- 
-  gtk_native_dialog_show(GTK_NATIVE_DIALOG(win32_native_file_chooser)) ;
+static void create_win32_native_file_chooser (GtkWidget *widget, gpointer user_data) {
+    GtkWidget *window = gtk_widget_get_toplevel(widget);
+    GtkFileChooserNative *win32_native_file_chooser = \
+        gtk_file_chooser_native_new("title", 
+        GTK_WINDOW(window), 
+        GTK_FILE_CHOOSER_ACTION_SAVE,
+        "accept label",
+        "cancel label");
+
+    gtk_native_dialog_show(GTK_NATIVE_DIALOG(win32_native_file_chooser));
 }
  
  
 int main (int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+    GtkWidget *window, *button;
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    button= gtk_button_new_with_label("test");
  
-  gtk_init(&argc, &argv) ;
+    g_signal_connect(button, "clicked", G_CALLBACK(create_win32_native_file_chooser), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
  
-  GtkWidget *window, *button ;
+    gtk_container_add(GTK_CONTAINER(window), button);
+    gtk_widget_show_all(window);
+    gtk_main();
  
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL) ;
- 
-  button= gtk_button_new_with_label("test") ;
- 
-  g_signal_connect(button, "clicked", G_CALLBACK(create_win32_native_file_chooser) , NULL);
-  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
- 
-  gtk_container_add(GTK_CONTAINER(window), button) ;
-  gtk_widget_show_all(window);
- 
-  gtk_main() ;
- 
-  return 0 ;
+    return 0;
 }
